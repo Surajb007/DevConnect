c3859ef281aa720a0616037a0185a1566670e3b1
Suraj Bondugula <surajb2996@gmail.com>
2020-04-24 01:21:08 -0400

    Setup Express Router

--- server.js
+++ server.js
@@ -2,9 +2,15 @@ const express = require('express'); // Bring Express
 const connectDB = require('./config/db');
 
 const app = express();
-connectDB();
+
+connectDB(); // Connect the database
 
 app.get('/', (req, res) => res.send('API running'));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profiles', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 
