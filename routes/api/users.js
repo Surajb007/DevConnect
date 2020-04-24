193ed0b953691d9edd937a4e1df2f8ea2530fe06
Suraj Bondugula <surajb2996@gmail.com>
2020-04-24 18:12:23 -0400

    Add code to save user to db

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,7 @@
 const express = require('express');
+const User = require('../../models/User');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
@@ -16,12 +19,38 @@ router.post(
       min: 6,
     }),
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    res.send(req.body);
+    const { name, email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) {
+        return res.status(400).json({
+          errors: [{ msg: 'User already Exists try logging in instead' }],
+        });
+      }
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+      });
+      user = new User({
+        email,
+        name,
+        avatar,
+        password,
+      });
+      const salt = await bcrypt.genSalt(10);
+      user.password = await bcrypt.hash(password, salt);
+      await user.save();
+      res.send('User Registered');
+    } catch (err) {
+      console.log(err.message);
+      res.status(500).send('Server Error');
+    }
   }
 );
 
