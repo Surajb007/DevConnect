2d1002dd252be264b99ebb3d9594e262876dd707
Suraj Bondugula <surajb2996@gmail.com>
2020-04-24 14:21:56 -0400

    Add express validators

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check, validationResult } = require('express-validator');
 
 const router = express.Router();
 
@@ -6,8 +7,22 @@ const router = express.Router();
 // @desc  Register User
 // @access Public
 
-router.post('/', (req, res) => {
-  res.send(req.body);
-});
+router.post(
+  '/',
+  [
+    check('name', 'Name is required').not().isEmpty(),
+    check('email', 'Email is invalid').isEmail(),
+    check('password', 'Password should be of minimum six characters').isLength({
+      min: 6,
+    }),
+  ],
+  (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    res.send(req.body);
+  }
+);
 
 module.exports = router;
