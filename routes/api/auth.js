6c3f7ffc4b92a4e5a9f3e7685d695e35ccaf5c1a
Suraj Bondugula <surajb2996@gmail.com>
2020-04-25 02:04:26 -0400

    Add Authentication Code to authenticate required routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,7 +1,8 @@
 const express = require('express');
+const auth = require('../../middleware/auth');
 
 const router = express.Router();
 
-router.get('/', (req, res) => res.send('Auth API'));
+router.get('/', auth, (req, res) => res.send('Auth API'));
 
 module.exports = router;
